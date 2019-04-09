b685dbf8840b49c394b13103857ae1e58e695b46
vamakin <vamakin@gmail.com>
2019-04-09 16:13:46 +0300

    add result class

--- src/StringPositionDetector.php
+++ src/StringPositionDetector.php
@@ -1,5 +1,9 @@
 <?php
 
+namespace Searcher;
+
+use Searcher\Results\Result;
+use Searcher\SearchTypes\iSearchType;
 
 class StringPositionDetector
 {
@@ -8,23 +12,33 @@ class StringPositionDetector
      * @var string
      */
     private $filename;
+    /**
+     * @var iSearchType
+     */
+    private $searchType;
 
-    public function __construct(string $filename)
+    public function __construct(string $filename, iSearchType $searchType)
     {
         $this->filename = $filename;
+        $this->searchType = $searchType;
     }
 
     public function search(string $needle)
     {
         $lineNumber = 0;
-        $handler = fopen($this->filename, 'r');
-        while(!feof($handler))  {
-            $line = fgets($handler);
-            $position = strpos($line, $needle);
-            if ($position !== false) {
-                return new Result($lineNumber, $position);
+        $handle = fopen($this->filename, 'r');
+        if ($handle) {
+            while(!feof($handle))  {
+                $line = fgets($handle);
+                $positionInLine = $this->searchType->search($line, $needle);
+                if ($positionInLine->isFound() !== false) {
+                    fclose($handle);
+                    return new Result($lineNumber, $positionInLine->position());
+                }
+                $lineNumber++;
             }
-            $lineNumber++;
+        } else {
+            //todo throw exception
         }
     }
 }
