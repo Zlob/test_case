5c2906bd9ebc4d90408246922b99d66dedd35ae4
vamakin <vamakin@gmail.com>
2019-04-09 14:10:06 +0300

    base searcher

---
+++ src/Result.php
@@ -0,0 +1,37 @@
+<?php
+
+
+class Result
+{
+    /**
+     * @var int
+     */
+    private $number;
+    /**
+     * @var int
+     */
+    private $position;
+
+    public function __construct(int $number, int $position)
+    {
+        $this->number = $number;
+        $this->position = $position;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumber(): int
+    {
+        return $this->number;
+    }
+
+}
---
+++ src/Searcher.php
@@ -0,0 +1,10 @@
+<?php
+
+
+class Searcher
+{
+    public function search(string $string, $filename)
+    {
+        return new Result(1, 1);
+    }
+}
